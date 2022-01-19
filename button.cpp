0cd7cbc394fa961156ae36c9cba7add960f844b4
skAdOOdlEs7 <grantdriddle7@gmailcom>
2022-01-18 21:09:01 -0700

    formatting

--- button.cpp
+++ button.cpp
@@ -1,60 +1,70 @@
 #include <SFML/Graphics.hpp>
 #include "button.hpp"
-        
-        Button::Button(sf::RenderWindow &win) : window(win){
-            
-        }
 
-        void Button::updateBounds(){
-            bounds = getGlobalBounds();
-        }
+Button::Button(sf::RenderWindow &win) : window(win)
+{
+}
 
-        
+void Button::updateBounds()
+{
+    bounds = getGlobalBounds();
+}
 
-        bool Button::gethovered(){
-            mouse_position = sf::Mouse::getPosition(window);
-            updateBounds();
-            if(bounds.contains(mouse_position.x, mouse_position.y)){
-                hovered = true;
-                return true;
+bool Button::gethovered()
+{
+    mouse_position = sf::Mouse::getPosition(window);
+    updateBounds();
+    if (bounds.contains(mouse_position.x, mouse_position.y))
+    {
+        hovered = true;
+        return true;
+    }
+    hovered = false;
+    return false;
+}
 
-            }
-            hovered = false;
-            return false;
-        }   
+bool Button::getclicked()
+{
+    if (gethovered())
+    {
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            clicked = true;
+            return true;
+        }
+    }
+    clicked = false;
+    return false;
+}
 
-        bool Button::getclicked(){
-            if(gethovered()){
-                if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-                    clicked = true;
-                    return true;
-                }
-            }
-            clicked = false;
-            return false;
-        }
+void Button::updatetexture()
+{
+    getclicked();
+    if (clicked)
+    {
+        setTexture(clickedTexture);
+    }
+    else if (hovered)
+    {
+        setTexture(hoveredTexture);
+    }
+    else
+    {
+        setTexture(baseTexture);
+    }
+}
 
-        void Button::updatetexture(){
-            getclicked();
-            if(clicked){
-                setTexture(clickedTexture);
-            }
-            else if(hovered){
-                setTexture(hoveredTexture);
-            }
-            else {
-                setTexture(baseTexture);
-            }
-        }
+void Button::setBaseTexture(sf::Texture texture)
+{
+    baseTexture = texture;
+}
 
-        void Button::setBaseTexture(sf::Texture texture){
-            baseTexture = texture;
-        }
-        
-        void Button::setHoverTexture(sf::Texture texture){
-            hoveredTexture = texture;
-        }
-        
-        void Button::setClickedTexture(sf::Texture texture){
-            clickedTexture = texture;
-        }
+void Button::setHoverTexture(sf::Texture texture)
+{
+    hoveredTexture = texture;
+}
+
+void Button::setClickedTexture(sf::Texture texture)
+{
+    clickedTexture = texture;
+}
